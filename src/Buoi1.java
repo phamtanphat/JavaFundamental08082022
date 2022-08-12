c9d345186697e62da5c4246920f557b805bfb328
phamtanphat <phatdroid94@gmail.com>
2022-08-12 21:29:06 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,15 @@
+public class Buoi1 {
+    // comment
+    // 1 : Kiểu dữ liệu
+    // Nhóm 1 (Nguyên thuỷ - primities): char, int, long, double, float, boolean
+    // Nhóm 2 (Đối tượng - Object): String, Integer, Long, Double, Float, Boolean
+
+    // 2: Khai báo biến tuân thủ theo camelCase
+    char a = 'a'; // char - kiểu ký tự chứa trong ''
+    int age = 28; // int, long - kiểu số nguyên (Số không có phần thập phân)
+    float height = 1.75f; // float, double - kiểu số có phần thập phân
+    boolean isMale = true; // boolean - kiểu luận lý
+
+}
+
+
