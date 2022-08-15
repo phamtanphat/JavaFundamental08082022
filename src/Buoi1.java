a27cb45c84cb08db81ce46e9785cf6427ca5e45f
phamtanphat <phatdroid94@gmail.com>
2022-08-15 20:46:16 +0700

    toan tu

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,14 +1,58 @@
 public class Buoi1 {
-    // comment
-    // 1 : Kiểu dữ liệu
-    // Nhóm 1 (Nguyên thuỷ - primities): char, int, long, double, float, boolean
-    // Nhóm 2 (Đối tượng - Object): String, Integer, Long, Double, Float, Boolean
+    public static void main(String[] args) {
+        // comment
+        // 1 : Kiểu dữ liệu
+        // Nhóm 1 (Nguyên thuỷ - primities): char, int, long, double, float, boolean
+        // Nhóm 2 (Đối tượng - Object): String, Integer, Long, Double, Float, Boolean
 
-    // 2: Khai báo biến tuân thủ theo camelCase
-    char a = 'a'; // char - kiểu ký tự chứa trong ''
-    int age = 28; // int, long - kiểu số nguyên (Số không có phần thập phân)
-    float height = 1.75f; // float, double - kiểu số có phần thập phân
-    boolean isMale = true; // boolean - kiểu luận lý
+        // 2: Khai báo biến tuân thủ theo camelCase
+        // char a = 'a'; // char - kiểu ký tự chứa trong ''
+        // int age = 28; // int, long - kiểu số nguyên (Số không có phần thập phân)
+        // float height = 1.75f; // float, double - kiểu số có phần thập phân
+        // boolean isMale = true; // boolean - kiểu luận lý
+
+        // String name = "Pham Tan Phat"; // String - kieu chuoi
+
+        // 3: Toan tu
+        // int a = 5;
+        // a = a + 1 <=> a += 1;
+        // int b = a++;
+
+        // Toan tu ++
+
+        // Truong hop 1: a++
+        // 1 - Truyen gia tri a vao vi tri a++
+        // 2 - Tang a len 1 don vi
+
+        // Truong hop 2: ++a
+        // 1 - Tang a len 1 don vi
+        // 1 - Truyen gia tri a vao vi tri a++
+
+        // System.out.println("Gia tri cua a: " + a);
+        // System.out.println("Gia tri cua b: " + b);
+
+        int a = 5;
+        int b = 3;
+
+        int result = a++ - --b + --a + a-- + b++ - b-- + a++ + --a;
+        // 5 - --b + --a + a-- + b++ - b-- + a++ + --a; a = 6, b = 3;
+        // 5 - 2 + --a + a-- + b++ - b-- + a++ + --a; a = 6, b = 2;
+        // 5 - 2 + 5 + a-- + b++ - b-- + a++ + --a; a = 5, b = 2;
+        // 5 - 2 + 5 + 5 + b++ - b-- + a++ + --a; a = 4, b = 2;
+        // 5 - 2 + 5 + 5 + 2 - b-- + a++ + --a; a = 4, b = 3;
+        // 5 - 2 + 5 + 5 + 2 - 3 + a++ + --a; a = 4, b = 2;
+        // 5 - 2 + 5 + 5 + 2 - 3 + 4 + --a; a = 5, b = 2;
+        // 5 - 2 + 5 + 5 + 2 - 3 + 4 + 4; a = 4, b = 2;
+        // a = 4, b = 2, result = 20
+
+        System.out.println("Gia tri cua a: " + a);
+        System.out.println("Gia tri cua b: " + b);
+        System.out.println("Gia tri cua ket qua: " + result);
+
+        // Value 1: a = 4, b = 2, result = 20 (Tich)
+        // Value 2: a = 4, b = 2, result = 23 (Kevin)
+        // Value 3: a = 5, b = 2, result = 17 (Tan)
+    }
 
 }
 
